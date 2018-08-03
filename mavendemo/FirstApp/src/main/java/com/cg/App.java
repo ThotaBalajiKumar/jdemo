c73ec52230c99c6b59cddac4418ff6149c308aee
Balaji Kumar <thota.balaji-kumar@capgemini.com>
2018-08-03 11:38:32 +0530

    balu comitted maven file

---
+++ mavendemo/FirstApp/src/main/java/com/cg/App.java
@@ -0,0 +1,17 @@
+package com.cg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!"+greetMe());
+    }
+	public static String greetMe()
+	{
+		return "Balaji";
+	}
+}
